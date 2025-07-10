7f33610cd9b72ecc72404721215c0bbfbcc8f900
J I H <jasondoudsema2@gmail.com>
2025-07-09 21:28:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ document.querySelectorAll('.emotion-box').forEach(box => {
     }, 1500);
   });
 });
+function checkUnlock() {
+  if (localStorage.getItem('darkUnlocked') === 'true') {
+    document.getElementById('lockedMessage').style.display = 'none';
+    document.getElementById('unlockedWords').style.display = 'block';
+  }
+}
+checkUnlock();
+
+function redirectToPayPal() {
+  window.open('https://www.paypal.me/JasonOudsema879/5.99', '_blank');
+}
+
+function enterPassword() {
+  const input = prompt('WHAT IS THE PASSWORD BRO??');
+  if (input === '0420') {
+    localStorage.setItem('darkUnlocked', 'true');
+    location.reload();
+  }
+}
